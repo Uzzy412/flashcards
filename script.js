75919dba2fed0be2db9c74b9b3887dfb786a7a7d
Alex <uzzy412@yahoo.com>
2025-01-01 18:59:38 +0200

    v2 selecting a profile feature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const container = document.querySelector(".container");
 const createProfileBtn = document.querySelector(".create-profile-btn");
 const profileNameInput = document.querySelector(".profile-name");
 
+let profile;
 let db;
 let request = indexedDB.open("flashcardsDatabase", 1);
 
@@ -10,9 +12,9 @@ request.onerror = function() {
 
 request.onupgradeneeded = function() {
   db = request.result;
-  if (!db.objectStoreNames.contains("profiles")) {
-    const profileStore = db.createObjectStore("profiles", { autoIncrement: true });  
-  }
+  const profileStore = db.createObjectStore("profiles", { autoIncrement: true });
+  const nameIndex = profileStore.createIndex("by_name", "profileName", { unique: true });
+  const dataIndex = profileStore.createIndex("by_data", "data", { unique: false });
 };
 
 request.onsuccess = function() {
@@ -24,7 +26,7 @@ request.onsuccess = function() {
 
 function searchForProfileExistence(db) {
   const tr = db.transaction("profiles");
-  tr.oncomplete = function() {console.log("Transaction complete");};
+  // tr.oncomplete = function() {console.log("Transaction complete");};
 
   const store = tr.objectStore("profiles");
   const search = store.count();
@@ -32,12 +34,47 @@ function searchForProfileExistence(db) {
   search.onsuccess = function() {
     let result = search.result;
     if (result === 1) {
-      console.log("There is a profile.", result);
+      console.log("There is a profile.");
     } else if (result > 1) {
-      console.log("There are more profiles", result);
+      console.log(`There are ${result} profiles`);
     } else {
       console.log("There is no profile. Please create one.");
-    }  
+    }
+    if (result >= 1) {
+      const select = document.createElement("select");
+      container.append(select);
+
+      const tr2 = db.transaction("profiles");
+      const store2 = tr2.objectStore("profiles");
+      let result2 = store2.getAll();
+      
+      result2.onsuccess = function() {
+        select.innerHTML = result2.result.map(profile => {
+          return `<option id="${profile.profileName}">${profile.profileName}</option>`;
+        });
+      }
+
+      select.addEventListener("change", (e) => {
+        db = request.result;
+        if (e.target.tag = "option") {
+          console.log("You chose: " + e.target.value + " profile");
+        }
+    
+        const tr = db.transaction("profiles");
+        const store = tr.objectStore("profiles");
+    
+        const index = store.index("by_name");
+        const search = index.get(e.target.value);
+    
+        search.onsuccess = function() {
+          console.log("Match!", search.result);
+        };
+    
+        search.onerror = function() {console.log("error")};
+        
+      });
+
+    }
   };
 
   search.onerror = function() {
@@ -52,13 +89,15 @@ function createProfile(db, name) {
 
   console.log(`${profileNameInput.value} profile was created!`);
   console.log(db.objectStoreNames.length);
+
+  profileNameInput.value = '';
 }
 
 createProfileBtn.addEventListener("click", function(db) {
   db = request.result;
   createProfile(db, profileNameInput.value);
+  location.reload();
 });
+  
 
-// console.log("No profile");
-// alert("You don't have a profile. Please create one.");
 
